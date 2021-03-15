f546d6c7462c13117d51552e1875a19d0776db32
Deep Saha <deepsaha288@yahoo.com>
2021-03-16 00:47:03 +0530

    serach tree particular value

--- BinarySearchTreeTest.java
+++ BinarySearchTreeTest.java
@@ -39,4 +39,29 @@ public class BinarySearchTreeTest {
         System.out.println("Size of Tree is "+size);
         Assertions.assertEquals(6,size);
     }
+
+    @Test
+    public void givenBinarySearch_particular_value() {
+        Node<Integer> nodeOne= new Node(56);
+        Node<Integer> nodeTwo = new Node(30);
+        Node<Integer> nodeThree = new Node(70);
+        Node<Integer> nodeFour = new Node<>(22);
+        Node<Integer>  nodeFive = new Node<>(40);
+        Node<Integer>  nodeSix = new Node<>(60);
+
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(nodeOne);
+        binarySearchTree.insert(nodeTwo);
+        binarySearchTree.insert(nodeThree);
+        binarySearchTree.insert(nodeFour);
+        binarySearchTree.insert(nodeFive);
+        binarySearchTree.insert(nodeSix);
+        binarySearchTree.inorder();
+
+        Node searchResult = binarySearchTree.search(22);
+        System.out.println("Found "+searchResult.value);
+        Assertions.assertEquals(22,searchResult.value);
+    }
+
 }
