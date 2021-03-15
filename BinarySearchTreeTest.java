8ed19c0ff62ecbdcd84f98beb670cc0f327ca5b5
Deep Saha <deepsaha288@yahoo.com>
2021-03-16 00:43:08 +0530

    find the size of tree

--- BinarySearchTreeTest.java
+++ BinarySearchTreeTest.java
@@ -1,3 +1,5 @@
+package com.bst;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +16,27 @@ public class BinarySearchTreeTest {
         boolean result = binarySearchTree.root.left.value.equals(30) && binarySearchTree.root.value.equals(56) && binarySearchTree.root.right.value.equals(70);
         Assertions.assertTrue(result);
         binarySearchTree.inorder();
+    }
+
+    @Test
+    public void givenValues_find_theSize() {
+        Node<Integer> nodeOne= new Node(56);
+        Node<Integer> nodeTwo = new Node(30);
+        Node<Integer> nodeThree = new Node(70);
+        Node<Integer> nodeFour = new Node<>(22);
+        Node<Integer>  nodeFive = new Node<>(40);
+        Node<Integer>  nodeSix = new Node<>(60);
+
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insert(nodeOne);
+        binarySearchTree.insert(nodeTwo);
+        binarySearchTree.insert(nodeThree);
+        binarySearchTree.insert(nodeFour);
+        binarySearchTree.insert(nodeFive);
+        binarySearchTree.insert(nodeSix);
+        binarySearchTree.inorder();
+        int size = binarySearchTree.size();
+        System.out.println("Size of Tree is "+size);
+        Assertions.assertEquals(6,size);
     }
 }
